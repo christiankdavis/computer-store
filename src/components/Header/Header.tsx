aadf83baa98c3f075dbf12b4454d6ec5eb252479
Christian Davis <cdavis20@stanford.edu>
2026-03-11 22:40:48 -0400

    add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,23 @@
+import { useTheme } from "../../context/ThemeContext";
+
 import "./Header.css";
 
 export interface HeaderProps {}
 
-export const Header = (props: HeaderProps) => {
-  return <div className="header">Header</div>;
+export const Header = ({}: HeaderProps) => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <header className="header">
+      <div className="header__spacer" />
+
+      <div className="header__title">Refurbished Computer Store</div>
+
+      <div className="header__actions">
+        <button className="header__theme-toggle" onClick={toggleTheme}>
+          {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
+        </button>
+      </div>
+    </header>
+  );
 };
